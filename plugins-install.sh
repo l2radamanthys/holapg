382ae82e921c02e226c3247653dabf1ced007807
Ricardo Quiroga <l2radamanthys@gmail.com>
2015-07-16 16:43:05 -0300

    minor update

--- plugins-install.sh
+++ plugins-install.sh
@@ -2,4 +2,8 @@
 # install plugins from app
 
 #geolocation
-cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-geolocation.git
+#cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-geolocation.git
+cordova plugin add cordova-plugin-geolocation
+#cordova plugin rm cordova-plugin-geolocation
+cordova plugin add cordova-plugin-camera
+#cordova plugin rm cordova-plugin-camera
